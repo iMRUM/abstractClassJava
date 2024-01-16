8e4687daad0af9afade110bf0c8e20f32f19c793
Imry <rodkin.imry@gmail.com>
2024-01-16 11:54:45 +0200

    Add files via upload

---
+++ Animal/src/Animal.java
@@ -0,0 +1,114 @@
+public abstract class Animal {
+    private int legs;
+    Food food;
+    //String name;
+    public Animal(int l, Food f) {
+        legs = l;
+        food = new Food(f);
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public String getFoodSource() {
+        return food.getSource();
+    }
+    public double getFoodQuantity() {
+        return food.getQuantity();
+    }
+
+    //    public Animal(int l, Food f, String n) {
+//        legs = l;
+//        food = new Food(f);
+//        this.name = n;
+//    }
+    public abstract void makeSound();
+}
+class Bird extends Animal{
+    int wings = 2;
+    public Bird(){
+        super(2, new Food("grains", 33.5));
+    }
+    @Override
+    public void makeSound(){
+        System.out.println("twit");
+    }
+    public void fly(){
+        System.out.println("fly");
+    }
+}
+class StrayCat extends Animal{
+    private String name;
+    public StrayCat(String n){
+        super(4, new Food("fish", 5.5));
+        this.name=n;
+    }
+    @Override
+    public void makeSound(){
+        System.out.println("meow");
+    }
+    public void sneak(){
+        System.out.println("sneak");
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+class Dog extends Animal{
+
+    private String owner;
+    private String name;
+    public Dog(String n, String o){
+        super(4, new Food("bonzo", 7.5));
+        this.owner = o;
+        this.name = n;
+    }
+    @Override
+    public void makeSound(){
+        System.out.println("haw");
+    }
+    public void play(){
+        System.out.println("play");
+    }
+    public String getOwner() {
+        return owner;
+    }
+
+    public void printOwner(){
+        System.out.println(getOwner());
+    }
+}
+class Food {
+    private double quantity;
+    private String source;
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+
+    public Food(String s, double q) {
+        this.quantity = q;
+        this.source = s;
+    }
+
+    public Food(Food food) {
+        this.quantity= food.getQuantity();
+        this.source = food.getSource();
+    }
+}
+
---
+++ Animal/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        Dog dog = new Dog("pepe", "momo");
+        StrayCat cat = new StrayCat("trent");
+        bird.fly();
+        System.out.println("bird, quantity:"+bird.getFoodQuantity()+" source:"+bird.getFoodSource());
+        bird.fly();
+        bird.makeSound();
+        System.out.println("dog, quantity:"+dog.getFoodQuantity()+" source:"+dog.getFoodSource());
+        dog.play();
+        dog.makeSound();
+        System.out.println("cat, quantity:"+cat.getFoodQuantity()+" source:"+cat.getFoodSource());
+        cat.sneak();
+        cat.makeSound();
+
+
+    }
+}
